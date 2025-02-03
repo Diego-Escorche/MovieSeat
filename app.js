dffbc6f528f094797352a3efb018362b2efe46b6
Diego-Escorche <diegoescorche11@gmail.com>
2025-02-03 18:05:51 -0300

    Finished the basic endpoints for the REST API.

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express');
-const app = express();
 const crypto = require('crypto'); // For unique ID generation
 
 const moviesJSON = require('./movies.json');
-const { validateMovie } = require('./schemas/moviesSchema');
+const {
+  validateMovie,
+  validatePartialMovie,
+} = require('./schemas/moviesSchema');
 
+const app = express();
 app.use(express.json());
 app.disable('x-powered-by');
 
 const PORT = process.env.PORT || 1234;
 
+// Common Methods for CORS: GET/HEAD/POST
+// Complex Methods for CORS: PUT/PATCH/DELETE -> They use CORS PRE-Flight or Options
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'https://movies.com',
+];
 // Movies by genre & by default all the movies available
 app.get('/movies', (req, res) => {
+  const origin = req.header('origin');
+  // When the request is from the same origin, it isn't sent on the request header.
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    // res.header('Access-Control-Allow-Origin', '*') => Allow Access to every Port. '*' means all available ports.
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   const { genre } = req.query;
   if (genre) {
     const filteredMovies = moviesJSON.filter((movie) =>
@@ -45,14 +62,64 @@ app.post('/movies', (req, res) => {
   }
 
   const newMovie = {
-    // Library that lets you create a new id. UUID = Universal unique identifier
-    id: crypto.randomUUID(),
-    ...result.data,
+    id: crypto.randomUUID(), // Creates the Universal Unique ID
+    ...result.data, // Gets the validated data
   };
 
   moviesJSON.push(newMovie);
 
   res.status(201).json(newMovie);
+});
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ message: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+  const movieIndex = moviesJSON.findIndex((m) => m.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' });
+  }
+
+  const updateMovie = {
+    ...moviesJSON[movieIndex],
+    ...result.data,
+  };
+
+  moviesJSON[movieIndex] = updateMovie;
+  return res.json(updateMovie);
+});
+
+app.delete('/movies/:id', (req, res) => {
+  const origin = req.header('origin');
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { id } = req.params;
+  const movieIndex = moviesJSON.find((m) => m.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ Error: 'Movie not found' });
+  }
+
+  moviesJSON.splice(movieIndex, 1);
+
+  return res.status(204).json({ message: 'Movie deleted successfully' });
+});
+
+app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin');
+
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // To allow access to the methods.
+  }
+  res.send();
 });
 app.listen(PORT, () => {
   console.log(`listening on port: http://localhost:${PORT}`);
--- schemas/moviesSchema.js
+++ schemas/moviesSchema.js
@@ -6,7 +6,7 @@ const movieSchema = z.object({
     invalid_type_error: 'Movie title must be a string.',
     required_error: 'Movie tittle is required.',
   }),
-  year: z.number().int().min(1900),
+  year: z.number().int(),
   director: z.string(),
   duration: z.number().int().positive(),
   rate: z.number().int().min(0).max(10),
@@ -37,6 +37,11 @@ function validateMovie(object) {
   return movieSchema.safeParse(object);
 }
 
+function validatePartialMovie(object) {
+  return movieSchema.partial().safeParse(object); // The partial() makes the properties optional when it validates them.
+}
+
 module.exports = {
   validateMovie,
+  validatePartialMovie,
 };
